0b845bdf2d9723ca5aa30fcab5ca803c51af2338
chase1k <chasekillorin@gmail.com>
2023-04-12 16:05:46 -0400

    Filtering Complete?

--- Code/filter_packets.py
+++ Code/filter_packets.py
@@ -1,3 +1,48 @@
-def filter() :
+def filter():
 	print('called filter function in filter_packets.py')
-	
+
+	filename = '../Captures/Node1.txt'
+	file = open(filename,'r')
+	line = file.readline()
+	newFile = open('../Captures/ICMP_Node1.txt','w')
+	filta(file, newFile, line)
+
+	filename = '../Captures/Node2.txt'
+	file = open(filename,'r')
+	line = file.readline()
+	newFile = open('../Captures/ICMP_Node2.txt','w')
+	filta(file, newFile, line)
+
+	filename = '../Captures/Node3.txt'
+	file = open(filename,'r')
+	line = file.readline()
+	newFile = open('../Captures/ICMP_Node3.txt','w')
+	filta(file, newFile, line)
+
+	filename = '../Captures/Node4.txt'
+	file = open(filename,'r')
+	line = file.readline()
+	newFile = open('../Captures/ICMP_Node4.txt','w')
+	filta(file, newFile, line)
+
+def filta(file, newFile, line):
+
+	while line:
+		if line.__contains__('Echo'):
+			newFile.write("No.     Time           Source                Destination           Protocol Length Info\n")
+
+			while line:
+
+				print(line)
+				newFile.write(line)
+				line = file.readline()	
+
+				if line.__contains__('No.'):
+					line = None
+					exit
+
+			exit
+		line = file.readline()
+	file.close()
+	newFile.close()
+    
