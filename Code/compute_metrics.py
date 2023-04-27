584cd2ce97610648606c9b8fcaeaa8149e3a3d5f
P4ral1ax <braydenjw00@gmail.com>
2023-04-27 12:52:28 -0400

    Final working code

--- Code/compute_metrics.py
+++ Code/compute_metrics.py
@@ -1,3 +1,5 @@
+import csv
+
 # Hardcode the IPs
 NODE_IPS = ["192.168.100.1", "192.168.100.2", "192.168.200.1", "192.168.200.2"]
 
@@ -52,9 +54,14 @@ def calculate_reply_delay(list, node_ip):
 	return(average_delay)
 
 
-def calculate_hop_count():
-	pass
-
+def calculate_hop_count(packets):
+	hops = 0
+	count = 0
+	for packet in packets:
+		hops += abs(int(packet[9]) - 129) # We find the hops with a little subtraction magic, we know ttl is 128 by default
+		count += 1
+	average_hop = hops/count
+	return(average_hop)
 
 def calculate_bytes(list, offset):
 	bytes = 0
@@ -63,24 +70,16 @@ def calculate_bytes(list, offset):
 	return(bytes)
 
 
-def compute_node_stats(packets, node_ip):
+def compute_node_stats(packets, node_ip, f):
 	# Initalize all the variables
 	echo_req_sent = []
 	echo_req_recv = []
 	echo_reply_sent = []
 	echo_reply_recv = []
-	
-	total_rtt = 0
-	total_delay = 0
 	hop_count = 0
-	
-	total_packets = len(packets)
 
-	# increment simple counters
+	## Sort Packets ##
 	for packet in packets:
-		## DEBUG ##
-		# packet = packet[0:12]
-		# print(packet)
 		## Node Sent ##
 		if packet[3] == node_ip:
 			if 'reply' in packet[10]:
@@ -94,45 +93,63 @@ def compute_node_stats(packets, node_ip):
 				echo_reply_recv.append(packet)
 			if 'request' in packet[10]:
 				echo_req_recv.append(packet)
-		
 		# Error
 		else:
 			print(f"Node IP mismatch : Packet # : {packet[1]}, Node # : {packet[0]}, SRC IP : {packet[3]}, DST IP : {packet[4]}")
 
-	# Print Simple Stats
-	print(f"Echo Requests Sent: {len(echo_req_sent)}")
-	print(f"Echo Requests Recived: {len(echo_req_recv)}")
-	print(f"Echo Replies Sent: {len(echo_reply_sent)}")	
-	print(f"Echo Replies Recieved: {len(echo_reply_recv)}")
-	print(f"Echo Requests Bytes Sent: {calculate_bytes(echo_req_sent, 0)}")
-	print(f"Echo Requests Bytes Recieved: {calculate_bytes(echo_req_recv, 0)}")
-	print(f"Echo Requests Data Sent: {calculate_bytes(echo_req_sent, 42)}")
-	print(f"Echo Requests Data Recieved: {calculate_bytes(echo_req_recv, 42)}")
+	### Calculate & Write Stats ###
+
+	# Basic stats
+	f.write("Echo Requests Sent,Echo Requests Received,Echo Replies Sent,Echo Replies Received\r\n")
+	total_req_sent = len(echo_req_sent)
+	total_reply_sent = len(echo_reply_sent)
+	total_req_recv = len(echo_req_recv)
+	total_reply_recv = len(echo_reply_recv)
+	f.write(f"{total_req_sent},{total_reply_sent},{total_req_recv},{total_reply_recv}\r\n")
+
+	f.write("Echo Request Bytes Sent (bytes),Echo Request Data Sent (bytes)\r\n")
+	request_bytes_sent = calculate_bytes(echo_req_sent, 0)
+	request_data_sent = calculate_bytes(echo_req_sent, 42)
+	f.write(f"{request_bytes_sent},{request_data_sent}\r\n")
+	
+	f.write("Echo Request Bytes Received (bytes),Echo Request Data Received (bytes)\r\n")
+	request_bytes_received = calculate_bytes(echo_req_recv, 0)
+	request_data_received = calculate_bytes(echo_req_recv, 42)
+	f.write(f"{request_bytes_received},{request_data_received}\r\n")
 	
 	
 	# Calcuate Other Stats
 	rtt = calculate_rtt(packets, node_ip) # returns (average, total)
-	print(f"\nAverage RTT (ms): {rtt[0]:.2f}")
 	throughput = calculate_bytes(echo_req_sent, 0) / rtt[1]
-	print(f"Echo Request Throughput (kB/sec): {throughput:.1f}")
 	goodput = calculate_bytes(echo_req_sent, 42) / rtt[1] 
-	print(f"Echo Request Goodput (kB/sec): {goodput:.1f}")
 	reply_delay = calculate_reply_delay(packets, node_ip)
-	print(f"Average Reply Delay (us): {reply_delay:.2f}")
+	hop_count = calculate_hop_count(echo_reply_recv)
 
-	# Calculate Hop Count
-	print(f"\nAverage Echo Request Hop Count : NULL")
+	f.write(f"\r\nAverage RTT (milliseconds),{rtt[0]:.2f}\r\n")
+	f.write(f"Echo Request Throughput (kB/sec),{throughput:.1f}\r\n")
+	f.write(f"Echo Request Goodput (kB/sec),{goodput:.1f}\r\n")
+	f.write(f"Average Reply Delay (microseconds),{reply_delay:.2f}\r\n")
+	f.write(f"Average Echo Request Hop Count,{hop_count:.2f}\r\n\r\n")
+	
 		
 
 # Node Packets contains an array for each node. In that array is an an array of packets.
 # Array format : [NODENUMBER, PACKETNUMBER, TIME, SRC, DEST, PROTOCOL, LEN, ID, SEQNUM(LE/BE), TTL, TYPE, PAIRING]
 # Example Data : ['N4', '1444', '1442.007091', '192.168.100.1', '192.168.200.2', 'ICMP', '642', '0x0001', '148/37888', '128', 'Echo (ping) reply', 'request in 1443']
 def compute(node_packets) :
+
+	# Open File
+	f = open("metrics.csv", "w")
+
 	print('called compute function in compute_metrics.py')
 	
 	# For each node pass in packets and IP of node
-	for node in range(0, 1):#len(node_packets)):
-		print(f"\nNode {node+1}\n")
-		compute_node_stats(node_packets[node], NODE_IPS[node])
+	for node in range(0, len(node_packets)):
+		f.write(f"Node {node+1}\r\n\r\n")
+		compute_node_stats(node_packets[node], NODE_IPS[node], f)
+	f.close()
+	print("Metrics written to \"metrics.csv\"")
 	return
+
+	
 
